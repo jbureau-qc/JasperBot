41a415c89cf91fc26b75e0e2007f5c14340ac16f
SADI-PC\Sadistik <bureauj88@gmail.com>
2018-12-17 00:31:15 -0500

    init

---
+++ Core/Data/CrafterRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JasperBot.Core.Data
+{
+    public enum CrafterRole
+    {
+        Scibe,
+        Blacksmith,
+        Tailor,
+        Tamer,
+        Alchemy,
+        Carpentry,
+        Chef
+    } 
+}
---
+++ Core/Data/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JasperBot.Core.Data
+{
+    public enum OrderStatus
+    {
+        Unassigned,
+        Assigned,
+        InProgress,
+        Ready,
+        Completed
+    }
+}
